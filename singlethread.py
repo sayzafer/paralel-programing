7e0895b9ad4c8f04df5a8478991b30b5f4a24327
Zafer Say <zafersay93@gmail.com>
2021-10-11 17:28:30 +0300

    first

---
+++ singlethread.py
@@ -0,0 +1,14 @@
+from bc import *
+from threading import Thread
+
+if __name__ == '__main__':
+    find_e = FindE()
+    tt = TicToc()
+    tt.tic()
+    n = 10000000
+    thread = Thread(target=find_e.find_euler, args=(n,))
+    thread.start()
+    thread.join()
+
+    print("e = ", find_e.value_of_e())
+    print("TIME = ", tt.toc())
